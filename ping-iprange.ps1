ebcb0a7499f67cb5462fc5ec8ec6a697eace398b
Martin Tobing <MartinTobing@users.noreply.github.com>
2020-07-20 16:44:53 +0700

    Create ping-iprange.ps1

---
+++ ping-iprange.ps1
@@ -0,0 +1,19 @@
+function Ping($ip)
+{
+    if (Test-Connection -ipaddres $ip -Count 1 -ErrorAction SilentlyContinue){
+        Write-Host "$ip is REPLY" -ForegroundColor Green
+    }
+    else{
+        Write-Host "$ip is DOWN" -ForegroundColor Red
+    }
+}
+
+function PingRange([string[]]$iprange)
+{
+    foreach ($ip in $iprange)
+    {
+        Ping($ip)
+    }
+
+}
+PingRange (1..254 | % {"10.230.5."+$_})
